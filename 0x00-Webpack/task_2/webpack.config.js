c6491d87eb69aa60ca356fd0f2a35a82e03671fc
Segun <lawalsegun72@gmail.com>
2023-10-23 23:22:50 +0100

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  performance: {
+    maxAssetSize: 100000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
