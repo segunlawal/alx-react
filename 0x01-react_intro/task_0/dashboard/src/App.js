4364db9d6068b27619929bf98b680260409dfb3e
Segun <lawalsegun72@gmail.com>
2023-10-24 21:29:12 +0100

    Basic application

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import holberton_logo from './holberton_logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
