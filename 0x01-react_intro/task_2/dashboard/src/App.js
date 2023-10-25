38bfe1d31a63c7d02bebaa1f342be528cd942582
Segun <lawalsegun72@gmail.com>
2023-10-25 22:26:44 +0100

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,15 +12,18 @@ function App() {
 
       <body className="App-body">
         <p>Login to access the full dashboard</p>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+
+        <div className="App-credentials">
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <button type="submit">OK</button>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
-        </div>
-        <button type="submit">Ok</button>
       </body>
 
       <footer className="App-footer">
