45d11906954f52299e9f570d9dfe7ab8e8f96fc8
Segun <lawalsegun72@gmail.com>
2023-10-26 05:29:57 +0100

    Add build script

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        // use: ['babel-loader'],
       },
     ],
   },
