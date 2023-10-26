9ab3cac78c8eaa1484e02e51776688310ebe5243
Segun <lawalsegun72@gmail.com>
2023-10-26 04:11:41 +0100

    Add tests

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,4 @@
-// setup file
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
