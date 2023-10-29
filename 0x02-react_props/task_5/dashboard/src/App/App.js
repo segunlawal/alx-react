384df913b91dde1d6795a23ab23a700db4fdf813
Segun <lawalsegun72@gmail.com>
2023-10-29 05:10:25 +0100

    Shapes, Loops, and Keys

---
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import './App.css';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: getLatestNotification() },
+];
+
+function App({ isLoggedIn }) {
+  return (
+    <>
+      <Notifications listNotifications={listNotifications} />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default App;
---
+++ 0x02-react_props/task_5/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import '../setupTests';
+
+describe('Notification Item', () => {
+  it('should render without crashing', () => {
+    const notificationItem = shallow(<NotificationItem />);
+    expect(notificationItem).toBeDefined;
+  });
+});
+
+describe('Notification Item prop', () => {
+  it('should render the correct props', () => {
+    const no = shallow(<NotificationItem type="default" value="test" />);
+    const li = no.find('li[data-notification-type]');
+    expect(li.prop('data-notification-type')).toBe('default');
+    expect(li.text()).toBe('test');
+  });
+});
+
+// describe('Notification Item props', () => {
+//   it('should render the correct prop for html', () => {
+//     const notificationItem = shallow(
+//       <NotificationItem type="default" html={{ __html: '<u>test</u>' }} />,
+//     );
+//     const li = notificationItem.find('li[data-notification-type]');
+//     expect(li.prop('data-notification-type')).toBe('default');
+//     expect(li.html()).toBe(
+//       '<li data-notification-type="default"><u>test</u></li>',
+//     );
+//   });
+// });
