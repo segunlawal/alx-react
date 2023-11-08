ce8ca4eb3140c25580dea317ec61bd8c24e73c0a
Segun <lawalsegun72@gmail.com>
2023-11-09 00:23:29 +0100

    Conditionally applying style

---
+++ 0x04-React_inline_styling/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const Login = () => {
+  return (
+    <>
+      <div className={css(styles.AppBody)}>
+        <p>Login to access the full dashboard</p>
+
+        <div className={css(styles.AppCredentials)}>
+          <div>
+            <label htmlFor="email" className={css(styles.label)}>
+              Email:
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className={css(styles.input)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className={css(styles.label)}>
+              Password:
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className={css(styles.input)}
+            />
+          </div>
+          <button type="submit" className={css(styles.submitButton)}>
+            OK
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  AppBody: {
+    marginTop: '50px',
+    marginLeft: '30px',
+  },
+
+  AppCredentials: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '2em',
+  },
+
+  input: {
+    marginLeft: '10px',
+  },
+
+  label: {
+    paddingRight: '2px',
+  },
+
+  submitButton: {
+    borderRadius: '5px',
+    outline: '0',
+    background: 'white',
+    padding: '2px 8px',
+    border: '1px solid gray',
+  },
+});
+
+export default Login;
---
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate = (nextProps) => {
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
+  };
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+
+    return (
+      <>
+        <div className={css(styles.menuItem)}>Your notifications</div>
+
+        {displayDrawer && (
+          <div className={css(styles.Notifications)}>
+            <button
+              type="button"
+              aria-label="Close"
+              onClick={() => {
+                console.log('Close button has been clicked');
+              }}
+              style={{
+                color: '#3a3a3a',
+                background: 'none',
+                border: 'none',
+                position: 'absolute',
+                right: '10px',
+                top: '10px',
+                cursor: 'pointer',
+                transform: 'scale(0.5)',
+              }}
+            >
+              <img src={closeIcon} alt="close" />
+            </button>
+            {listNotifications.length > 0 && (
+              <p>Here is the list of notifications</p>
+            )}
+            <ul>
+              {listNotifications.length === 0 ? (
+                <NotificationItem value="No new notification for now" />
+              ) : (
+                listNotifications.map(({ id, html, type, value }) => (
+                  <NotificationItem
+                    key={id}
+                    type={type}
+                    value={value}
+                    html={html}
+                    markAsRead={this.markAsRead}
+                    id={id}
+                  />
+                ))
+              )}
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+const styles = StyleSheet.create({
+  Notifications: {
+    border: '2px dotted red',
+    padding: '10px',
+    position: 'absolute',
+    top: '2.5em',
+    right: '10px',
+  },
+  menuItem: {
+    textAlign: 'right',
+  },
+});
+
+export default Notifications;
