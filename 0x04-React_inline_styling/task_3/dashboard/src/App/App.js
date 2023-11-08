53393e42b280c9de66e55dd26eeb59dc168f142d
Segun <lawalsegun72@gmail.com>
2023-11-09 00:57:03 +0100

    Responsive design

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/App/App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { StyleSheet, css } from 'aphrodite';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: getLatestNotification() },
+];
+
+class App extends React.Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+    logOut: PropTypes.func,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+    logOut: () => {},
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={css(styles.App)}>
+          <div className="root-notifications">
+            <Notifications listNotifications={listNotifications} />
+          </div>
+          <div className="App">
+            <Header />
+            <div className="App-body">
+              {this.props.isLoggedIn ? (
+                <BodySectionWithMarginBottom title="Course List">
+                  <CourseList listCourses={listCourses} />
+                </BodySectionWithMarginBottom>
+              ) : (
+                <BodySectionWithMarginBottom title="Log in to continue">
+                  <Login />
+                </BodySectionWithMarginBottom>
+              )}
+              <BodySection title="News from the school">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.
+                </p>
+              </BodySection>
+            </div>
+
+            <div>
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  App: {
+    fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
+  },
+});
+
+export default App;
